2b81dd568ccd42763d219937e0cfd7f05b29c544
BartoszPatro <161605900+BartoszPatro@users.noreply.github.com>
2024-05-21 10:37:16 +0000

    Commit4_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,19 +8,40 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      Scanner scanner=new Scanner(System.in);
 
+      System.out.println("Wybierz opcję:");
+      System.out.println("1. Dodaj nowego studenta");
+
+      int opcja=scanner.nextInt();
+      scanner.nextLine();
+
+      switch(opcja)
+      {
+        case 1:
+          System.out.println("Podaj imię nowego studenta: ");
+          String imie=scanner.nextLine();
+
+          System.out.println("Podaj wiek nowego studenta: ");
+          int wiek=scanner.nextInt();
+          scanner.nextLine();
+
+          s.addStudent(new Student(imie, wiek));
+          break;
+          
+      }
       var students = s.getStudents();
       for(Student current : students) {
         System.out.println(current.ToString());
       }
     } catch (IOException e) {
+      e.printStackTrace();
 
     }
   }
